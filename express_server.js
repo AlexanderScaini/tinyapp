66cdb7e8bf994119ea54cafec61574134c82cb5f
Alexander Scaini <alexanderscaini@gmail.com>
2020-09-28 18:52:52 +0000

    added ejs to dependencies

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3080; // default port 3080
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
